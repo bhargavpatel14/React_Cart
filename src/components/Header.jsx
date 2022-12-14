9a0d926144b954a87b76f028a2cfa7995988dc81
Bhargav patel <bhargavpatel@Bhargavs-MacBook-Pro.local>
2022-12-14 18:50:02 -0500

    Logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
+import { useSelector } from "react-redux";
 
-function Header() {
+const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <nav>
-      <h2>Logo.</h2>
+      <h2>Bhargav Patel.</h2>
+
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/cart"}>
           <FiShoppingBag />
-          <p>{0}</p>
+          <p>{cartItems.length}</p>
         </Link>
       </div>
     </nav>
   );
-}
+};
 
 export default Header;
