a7af536e131cf8113173d81910267e6e6f57bbb3
Bhargav patel <bhargavpatel@Bhargavs-MacBook-Pro.local>
2022-12-14 16:19:27 -0500

    Created Home , Header Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import logo from "./logo.svg";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Home from "./components/Home";
+import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
+import Cart from "./components/Cart";
 
 import "./Styles/app.scss";
 
 function App() {
-  return;
-  <BrowserRouter>
-    <Header>
+  return (
+    <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
-    </Header>
-  </BrowserRouter>;
+      <Toaster />
+    </BrowserRouter>
+  );
 }
 
 export default App;
